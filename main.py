349925433e1b976be20ad7569b79884655de7591
Reena Koshta <reenakoshta@yahoo.com>
2021-10-06 14:16:11 +0200

    added condition if user entered invalid number of players

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from utils.game import Board
 
 
 board = Board()
-number_of_players = int(input("How many players wants to play game:"))
+number_of_players = input("How many players wants to play game:")
+
+while number_of_players.isnumeric() == False:
+    number_of_players= input("Please enter a valid number: ")
+else:
+    number_of_players= int(number_of_players)
 board.start_game(number_of_players)
