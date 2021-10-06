0ceafadf46bb16652bded2e75b22f73a7c3605ce
Reena Koshta <reenakoshta@yahoo.com>
2021-10-06 15:01:25 +0200

    added error handling  invalid number of player has been entered

--- main.py
+++ main.py
@@ -2,5 +2,16 @@ from utils.game import Board
 
 
 board = Board()
-number_of_players = int(input("How many players wants to play game:"))
+number_of_players = input("How many players wants to play game:")
+
+# Condition to check weather user entered valid number of players or not
+# if number_of_players in not valid it will keep on asking for the valid
+# number until user enter the correct one.
+
+while number_of_players.isnumeric() == False:
+    number_of_players = input("Please enter a valid number: ")
+else:
+    number_of_players = int(number_of_players)
+
+# Start the game
 board.start_game(number_of_players)
